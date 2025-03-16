c519f9b8bccb3d8dbcae4bcfc12b3554aef94d8f
snips66 <kippus@windowslive.com>
2025-03-16 23:08:52 +0200

    Spring Boot ülesseadmine

---
+++ src/main/java/org/flightplanner/flightplanner/FlightPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.flightplanner.flightplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/flightplanner/flightplanner/FlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flightplanner.flightplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
